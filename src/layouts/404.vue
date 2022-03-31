dd639e0b263d18ad4d91c19f04449c80434a68d7
martindonadieu <martindonadieu@gmail.com>
2022-03-31 04:21:31 +0100

    fix: 404

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,26 +1,41 @@
 <script setup lang="ts">
+import Crisp from '~/services/crisp'
+
 useHead({
   meta: [
     { name: 'robots', content: 'noindex, nofollow' },
   ],
 })
+
+const crisp = Crisp()
+const openSupport = () => {
+  crisp.openMessenger()
+}
+
 </script>
+
 <template>
-  <!-- https://images.unsplash.com/photo-1517963628607-235ccdd5476c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3571&q=80 -->
-  <main class="min-h-full bg-cover bg-top sm:bg-top" style="background-image: url('https://images.unsplash.com/photo-1517963628607-235ccdd5476c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=0&con=0&sat=0')">
-    <div class="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-      <p class="text-sm font-semibold text-gray-300 uppercase tracking-wide">
-        404 error
-      </p>
-      <h1 class="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-        Uh oh! I think you’re lost.
-      </h1>
-      <p class="mt-2 text-lg font-medium text-gray-300">
-        It looks like the page you’re looking for doesn't exist.
-      </p>
-      <div class="mt-6">
-        <a href="/" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white"> Go back home </a>
-      </div>
+  <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div class="max-w-max mx-auto">
+      <main class="sm:flex">
+        <p class="text-4xl font-extrabold text-cornflower-600 sm:text-5xl">
+          404
+        </p>
+        <div class="sm:ml-6">
+          <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+              Page not found
+            </h1>
+            <p class="mt-1 text-base text-gray-500">
+              Please check the URL in the address bar and try again.
+            </p>
+          </div>
+          <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <a href="/" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-cornflower-600 hover:bg-cornflower-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cornflower-500"> Go back home </a>
+            <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-cornflower-700 bg-cornflower-100 hover:bg-cornflower-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cornflower-500" @click="openSupport"> Contact support </a>
+          </div>
+        </div>
+      </main>
     </div>
-  </main>
+  </div>
 </template>
