e43696828ba677c363c26394ed459891116c1d71
martindonadieu <martindonadieu@gmail.com>
2022-04-03 02:34:49 +0100

    fix: use new pricing

--- src/pages/pricing.vue
+++ src/pages/pricing.vue
@@ -30,7 +30,7 @@
                   $
                 </p>
                 <p class="text-4xl ml-0.5 font-bold text-gray-900 font-pj">
-                  9
+                  14
                 </p>
                 <p class="text-lg font-bold text-gray-400 font-pj">
                   /month
@@ -137,7 +137,7 @@
                     $
                   </p>
                   <p class="text-4xl ml-0.5 font-bold text-gray-900 font-pj">
-                    29
+                    39
                   </p>
                   <p class="text-lg font-bold text-gray-400 font-pj">
                     /month
@@ -255,7 +255,7 @@
                 <svg class="w-5 h-5 mr-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                Unlimited Apps
+                10 Apps
               </li>
 
               <li class="flex items-center">
@@ -269,7 +269,7 @@
                 <svg class="w-5 h-5 mr-2.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                Unlimited Monthly Live Updates
+                250,000 Monthly Live Updates
               </li>
 
               <li class="flex items-center">
