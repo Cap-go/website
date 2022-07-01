fd54cef250974bc33424729b18c67c1b6199e7fd
martindonadieu <martindonadieu@gmail.com>
2022-07-01 18:44:59 +0100

    feat: add uptime endpoint

---
+++ netlify/functions/up.ts
@@ -0,0 +1,24 @@
+import type { Handler } from '@netlify/functions'
+import { useSupabase } from '../services/supabase'
+import { findEnv, getRightKey, sendRes, transformEnvVar } from './../services/utils'
+import type { definitions } from '~/types/supabase'
+
+interface Params {
+  service: string
+}
+
+export const handler: Handler = async(event) => {
+  const body = event.queryStringParameters as any as Params
+  const supabase = useSupabase(getRightKey(findEnv(event.rawUrl), 'supa_url'), transformEnvVar(findEnv(event.rawUrl), 'SUPABASE_ADMIN_KEY'))
+  if (body.service === 'database') {
+    const { data, error } = await supabase
+      .from<definitions['apps']>('apps')
+      .select()
+      .eq('app_id', 'unknow.unknow')
+      .single()
+    if (data && !error)
+      return sendRes()
+    else
+      return sendRes({ error: 'db not answering as expected' }, 500)
+  }
+}
