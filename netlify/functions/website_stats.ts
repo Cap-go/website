1c2c229a19ea65fa37fab4589c65815a9b4d6c06
martindonadieu <martindonadieu@gmail.com>
2022-07-18 18:20:17 +0100

    fix: date slide

--- netlify/functions/website_stats.ts
+++ netlify/functions/website_stats.ts
@@ -3,22 +3,22 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
 import type { Handler } from '@netlify/functions'
 import { useSupabase } from 'netlify/services/supabase'
-import { definitions } from '~/types/supabase'
 import { findEnv, getRightKey, sendRes, transformEnvVar } from './../services/utils'
+import type { definitions } from '~/types/supabase'
 
-const get = async (supabase: SupabaseClient) => {
-  const date_id = new Date().toISOString().slice(0, 7)
+const get = async(supabase: SupabaseClient) => {
+  const date_id = new Date().toISOString().slice(0, 10)
   const { data, error } = await supabase
-  .from<definitions['global_stats']>('global_stats')
-  .select()
-  .eq('date_id', date_id)
-  .single()
-  if(data && !error)
+    .from<definitions['global_stats']>('global_stats')
+    .select()
+    .eq('date_id', date_id)
+    .single()
+  if (data && !error)
     return sendRes(data)
   return sendRes({
     apps: 190,
     updates: 130000,
-    stars: 125
+    stars: 125,
   })
 }
 
@@ -30,7 +30,7 @@ export const handler: Handler = async(event) => {
 
   const supabase = useSupabase(getRightKey(findEnv(event.rawUrl), 'supa_url'), transformEnvVar(findEnv(event.rawUrl), 'SUPABASE_ADMIN_KEY'))
   if (event.httpMethod === 'GET')
-  return get(supabase)
+    return get(supabase)
   console.error('Method not allowed')
   return sendRes({ status: 'Method now allowed' }, 400)
 }
