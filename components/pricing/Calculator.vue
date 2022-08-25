b8d2235aaba1688bc2c2f45f8b1c37f09e7e6de5
martindonadieu <martindonadieu@gmail.com>
2022-08-25 21:34:39 +0100

    fix: use better default val

--- components/pricing/Calculator.vue
+++ components/pricing/Calculator.vue
@@ -25,7 +25,7 @@ const team = props.pricing.find(plan => plan.name === 'Team')!
 const mau = ref(props.paygBase.mau)
 const storage = ref(props.paygBase.storage)
 const updatesByMonth = ref(1)
-const updatesSize = ref(5)
+const updatesSize = ref(10)
 
 const updates = ref(mau.value * updatesByMonth.value)
 const bandwidth = ref(updates.value * updatesSize.value / 1000)
