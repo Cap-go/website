a144924c8fa1fc5aa40092deb04f73981df04b23
martindonadieu <martindonadieu@gmail.com>
2022-09-24 05:50:15 +0200

    feat: change footer and add imprint

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,6 @@
+<!-- eslint-disable vue/one-component-per-file -->
 <script setup lang="ts">
+import { defineComponent, h } from 'vue'
 import { openMessenger } from '~/services/crisp'
 const router = useRouter()
 router.afterEach((to) => {
@@ -8,116 +10,181 @@ router.afterEach((to) => {
 router.currentRoute.value.hash.startsWith('#support') && openMessenger()
 const config = useRuntimeConfig()
 const brand = config.brand
+const year = new Date().getFullYear()
+// no-rel target="_blank"
+const navigation = {
+  solutions: [
+    { name: 'Community', href: 'https://discord.gg/VnYRvBfgA6', target: '_blank' },
+    { name: 'App mobile', href: '/app_mobile/' },
+    { name: 'Documentation', href: 'https://docs.capgo.app/', target: '_blank' },
+    // { name: 'Commerce', href: '#' },
+    // { name: 'Insights', href: '#' },
+  ],
+  support: [
+    { name: 'Pricing', href: '/pricing/' },
+    { name: 'Guides', href: '/blog/' },
+    { name: 'API Status', href: 'https://status.capgo.app/', target: '_blank' },
+    { name: 'Chat', href: '#support' },
+  ],
+  company: [
+    // { name: 'About', href: '#' },
+    // { name: 'Blog', href: '#' },
+    // { name: 'Jobs', href: '#' },
+    // { name: 'Press', href: '#' },
+    // { name: 'Partners', href: '#' },
+    { name: 'Imprint', href: '/imprint/' },
+  ],
+  legal: [
+    // { name: 'Claim', href: '#' },
+    { name: 'Privacy', href: '/privacy/' },
+    { name: 'Terms', href: '/tos/' },
+  ],
+  hero: [
+    {
+      name: '5% of our revenue go to carbon removal',
+      href: 'https://climate.stripe.com/vxDf62',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24', class: 'inline w-6 h-6 text-blue-600' }, [
+            h('path', {
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': 2,
+              'd': 'M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: '100% open-source',
+      href: 'https://github.com/Cap-go',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24', class: 'inline w-6 h-6 text-blue-600' }, [
+            h('path', {
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': 2,
+              'd': 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'Built with Supabase',
+      href: 'https://supabase.com/',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24', class: 'inline w-6 h-6 text-blue-600' }, [
+            h('path', {
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': 2,
+              'd': 'M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'Build in public in Twitter',
+      href: 'https://twitter.com/Capgo_app',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24', class: 'inline w-6 h-6 text-blue-600' }, [
+            h('path', {
+              'stroke-linecap': 'round',
+              'stroke-linejoin': 'round',
+              'stroke-width': 2,
+              'd': 'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84',
+            }),
+          ]),
+      }),
+    },
+  ],
+}
 </script>
 
 <template>
-  <footer class="bg-white">
-    <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-      <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-        <div class="px-5 py-2">
-          <NuxtLink to="/app_mobile/" class="text-base text-gray-500 hover:text-gray-900">
-            App
-          </NuxtLink>
+  <footer class="bg-white" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">
+      Footer
+    </h2>
+    <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div class="space-y-8 xl:col-span-1">
+          <!-- <img class="h-10" src="https://tailwindui.com/img/logos/mark.svg?color=gray&shade=300" alt="Company name"> -->
+          <p class="text-base text-gray-500">
+            Making the world a better place through constructing elegant apps.
+          </p>
+          <ul role="list" class="mt-4 space-y-4">
+            <li v-for="item in navigation.hero" :key="item.name">
+              <NuxtLink :to="item.href" target="_blank" no-rel>
+                <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+                <span class="ml-3 text-base font-bold text-gray-400"> {{ item.name }} </span>
+              </NuxtLink>
+            </li>
+          </ul>
         </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="/pricing/" class="text-base text-gray-500 hover:text-gray-900">
-            Pricing
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="/blog/" class="text-base text-gray-500 hover:text-gray-900">
-            Blog
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="https://docs.capgo.app/" no-rel target="_blank" class="text-base text-gray-500 hover:text-gray-900">
-            Documentation
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="/privacy/" class="text-base text-gray-500 hover:text-gray-900">
-            Privacy
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#support" class="text-base text-gray-500 hover:text-gray-900"> Support </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="https://status.capgo.app/" no-rel target="_blank" class="text-base text-gray-500 hover:text-gray-900">
-            Status
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink to="/tos/" class="text-base text-gray-500 hover:text-gray-900">
-            Terms of services
-          </NuxtLink>
-        </div>
-
-        <div class="px-5 py-2">
-          <NuxtLink
-            to="https://discord.gg/VnYRvBfgA6"
-            target="_blank"
-            title="Join discord" class="text-base text-gray-500 hover:text-gray-900"
-          >
-            Join community
-          </NuxtLink>
-        </div>
-      </nav>
-      <section class="py-12 bg-white sm:py-6">
-        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div class="flex flex-col px-12 space-y-6 sm:max-w-xs sm:mx-auto sm:px-0 lg:flex-row lg:items-center lg:justify-center lg:max-w-none lg:space-x-12 lg:space-y-0">
-            <div class="flex items-center">
-              <svg class="flex-shrink-0 w-6 h-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
-              </svg>
-              <span class="ml-3 text-base font-bold text-gray-400"> Built with Supabase </span>
+        <div class="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-base font-medium text-gray-900">
+                Solutions
+              </h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li v-for="item in navigation.solutions" :key="item.name">
+                  <NuxtLink :to="item.href" :target="item.target" no-rel class="text-base text-gray-500 hover:text-gray-900">
+                    {{ item.name }}
+                  </NuxtLink>
+                </li>
+              </ul>
             </div>
-
-            <span class="block w-auto h-px bg-gray-200 lg:bg-gray-400 lg:w-px lg:h-4" />
-
-            <div class="flex items-center">
-              <svg class="flex-shrink-0 w-6 h-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-              </svg>
-              <span class="ml-3 text-base font-bold text-gray-400"> 100% Open source </span>
+            <div class="mt-12 md:mt-0">
+              <h3 class="text-base font-medium text-gray-900">
+                Support
+              </h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li v-for="item in navigation.support" :key="item.name">
+                  <NuxtLink :to="item.href" :target="item.target" no-rel class="text-base text-gray-500 hover:text-gray-900">
+                    {{ item.name }}
+                  </NuxtLink>
+                </li>
+              </ul>
             </div>
-
-            <span class="block w-auto h-px bg-gray-200 lg:bg-gray-400 lg:w-px lg:h-4" />
-
-            <div class="flex items-center">
-              <svg class="flex-shrink-0 w-6 h-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg>
-              <span class="ml-3 text-base font-bold text-mutedBlue-400"> <NuxtLink no-rel to="https://climate.stripe.com/vxDf62">5% of our revenue go to carbon removal</NuxtLink></span>
+          </div>
+          <div class="md:grid md:grid-cols-2 md:gap-8">
+            <div>
+              <h3 class="text-base font-medium text-gray-900">
+                Company
+              </h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li v-for="item in navigation.company" :key="item.name">
+                  <NuxtLink :to="item.href" class="text-base text-gray-500 hover:text-gray-900">
+                    {{ item.name }}
+                  </NuxtLink>
+                </li>
+              </ul>
+            </div>
+            <div class="mt-12 md:mt-0">
+              <h3 class="text-base font-medium text-gray-900">
+                Legal
+              </h3>
+              <ul role="list" class="mt-4 space-y-4">
+                <li v-for="item in navigation.legal" :key="item.name">
+                  <NuxtLink :to="item.href" class="text-base text-gray-500 hover:text-gray-900">
+                    {{ item.name }}
+                  </NuxtLink>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-      </section>
-      <div class="mt-8 flex justify-center space-x-6">
-        <NuxtLink to="https://twitter.com/Capgo_app" no-rel target="_blank" alt="Open twitter" class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">Twitter</span>
-          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-          </svg>
-        </NuxtLink>
-
-        <NuxtLink to="https://github.com/Cap-go" no-rel target="_blank" alt="Open GitHub" class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">GitHub</span>
-          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
-          </svg>
-        </NuxtLink>
       </div>
-      <p class="mt-8 text-center text-base text-gray-400">
-        &copy; 2022 {{ brand }}, Inc. All rights reserved.
-      </p>
+      <div class="mt-12 border-t border-gray-200 pt-8">
+        <p class="text-base text-gray-400 xl:text-center">
+          &copy; {{ year }} {{ brand }}, Inc. All rights reserved.
+        </p>
+      </div>
     </div>
   </footer>
 </template>
---
+++ pages/imprint.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative bg-indigo-800">
+    <div class="absolute inset-0">
+      <div class="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
+    </div>
+    <div class="relative mx-auto max-w-7xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+      <h1 class="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">
+        Get in touch
+      </h1>
+      <p class="mt-6 max-w-3xl text-xl text-indigo-100">
+        Company name: Digital shift OÜ <br>
+        Register code : 14236508 <br>
+        Address: Sepapaja 6, 15551 , Tallinn, Estonia <br>
+        Hosting: Netlify egde <br>
+        Data storage: AWS eu-west-2 (London) <br>
+        Data processing: Cloudflare edge and Netlify edge <br>
+      </p>
+    </div>
+  </div>
+</template>
