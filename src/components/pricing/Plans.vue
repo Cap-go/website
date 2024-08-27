26d0572cc5270f9e76605a2e3291a568873f315c
Martin Donadieu <martindonadieu@gmail.com>
2024-08-27 18:43:49 +0100

    fix: add plugin in pricing

--- src/components/pricing/Plans.vue
+++ src/components/pricing/Plans.vue
@@ -165,6 +165,14 @@ function descToEmoji(desc: string) {
                 ><span class="font-bold">{{ numberWithSpaces(plan.storage) }}</span> GB of Storage</span
               >
             </li>
+            <li class="flex items-center">
+              <svg class="w-5 h-5 mr-2 text-blue-600 shrink-0" xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="1em" viewBox="0 0 448 512">
+                <path
+                  d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+                />
+              </svg>
+              <span>Priority support for all Capgo plugins <span class="font-bold">(30+ plugins)</span></span>
+            </li>
           </ul>
         </div>
       </div>
