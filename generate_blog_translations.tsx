2d5d118fc96452bd8c749f72bc5c87239203fcfa
Rishi Raj Jain <jain71000@gmail.com>
2024-10-03 00:53:56 +0530

    use gpy

--- generate_blog_translations.tsx
+++ generate_blog_translations.tsx
@@ -2,73 +2,56 @@ import { appendFileSync, existsSync, mkdirSync, readdirSync, readFileSync, write
 import matter from 'gray-matter'
 import { join } from 'path'
 import { defaultLocale, locales } from './src/services/locale'
+import { translateText } from './translate'
 
 const languages = locales.filter((lang) => lang !== defaultLocale)
 const contentDirectory = join(process.cwd(), 'src', 'content')
 const blogDirectory = join(contentDirectory, 'blog')
 
-const translateBlogFiles = async () => {
-  const translationPromises = languages.map(async (lang) => {
-    console.log(`Preparing the blogs for locale: ${lang}...`)
-    const langBlogDirectory = join(contentDirectory, lang, 'blog')
-    if (!existsSync(langBlogDirectory)) mkdirSync(langBlogDirectory, { recursive: true })
-    const blogFiles = readdirSync(blogDirectory)
-    for (const file of blogFiles) {
-      const filePath = join(blogDirectory, file)
-      const destinationPath = join(langBlogDirectory, file)
-      writeFileSync(destinationPath, '', 'utf8')
-      const content = readFileSync(filePath, 'utf8')
-      const grayMatterEnd = content.indexOf('---', 4)
-      const { data: grayMatterJson } = matter(content)
-      if (grayMatterJson.title) {
-        const translatedTitle = await translateText(grayMatterJson.title, lang)
-        if (translatedTitle) grayMatterJson['title'] = translatedTitle
-      }
-      if (grayMatterJson.description) {
-        const translatedTitle = await translateText(grayMatterJson.description, lang)
-        if (translatedTitle) grayMatterJson['description'] = translatedTitle
-      }
-      grayMatterJson['locale'] = lang
-      appendFileSync(destinationPath, matter.stringify('', grayMatterJson), 'utf8')
-      const blogContent = content.substring(grayMatterEnd + 4)
-      const codeBlockRegex = /```[\s\S]*?```/g
-      const codeBlocks = [...blogContent.matchAll(codeBlockRegex)]
-      const blogContentWithoutCodeBlocks = blogContent.replace(codeBlockRegex, '[[CODE_BLOCK]]')
-      const sentences = blogContentWithoutCodeBlocks.split('.')
-      let currentChunk = ''
-      for (const sentence of sentences) {
-        if ((currentChunk + sentence).length > 4000) {
-          const tmp = await translateText(currentChunk, lang)
-          if (tmp) appendFileSync(destinationPath, tmp, 'utf8')
-          currentChunk = sentence
-        } else currentChunk += sentence
-      }
-      if (currentChunk) {
+for (const lang of languages) {
+  console.log(`Preparing the blogs for locale: ${lang}...`)
+  const langBlogDirectory = join(contentDirectory, lang, 'blog')
+  if (!existsSync(langBlogDirectory)) mkdirSync(langBlogDirectory, { recursive: true })
+  const blogFiles = readdirSync(blogDirectory)
+  for (const file of blogFiles) {
+    const filePath = join(blogDirectory, file)
+    const destinationPath = join(langBlogDirectory, file)
+    writeFileSync(destinationPath, '', 'utf8')
+    const content = readFileSync(filePath, 'utf8')
+    const grayMatterEnd = content.indexOf('---', 4)
+    const { data: grayMatterJson } = matter(content)
+    if (grayMatterJson.title) {
+      const translatedTitle = await translateText(grayMatterJson.title, lang)
+      if (translatedTitle) grayMatterJson['title'] = translatedTitle
+    }
+    if (grayMatterJson.description) {
+      const translatedTitle = await translateText(grayMatterJson.description, lang)
+      if (translatedTitle) grayMatterJson['description'] = translatedTitle
+    }
+    grayMatterJson['locale'] = lang
+    appendFileSync(destinationPath, matter.stringify('', grayMatterJson), 'utf8')
+    const blogContent = content.substring(grayMatterEnd + 4)
+    const codeBlockRegex = /```[\s\S]*?```/g
+    const codeBlocks = [...blogContent.matchAll(codeBlockRegex)]
+    const blogContentWithoutCodeBlocks = blogContent.replace(codeBlockRegex, '[[CODE_BLOCK]]')
+    const sentences = blogContentWithoutCodeBlocks.split('.')
+    let currentChunk = ''
+    for (const sentence of sentences) {
+      if ((currentChunk + sentence).length > 4000) {
         const tmp = await translateText(currentChunk, lang)
         if (tmp) appendFileSync(destinationPath, tmp, 'utf8')
-      }
-      let translatedContent = await readFileSync(destinationPath, 'utf8')
-      codeBlocks.forEach((match, index) => {
-        translatedContent = translatedContent.replace('[[CODE_BLOCK]]', match[0])
-      })
-      writeFileSync(destinationPath, translatedContent, 'utf8')
+        currentChunk = sentence
+      } else currentChunk += sentence
     }
-  })
-  await Promise.all(translationPromises)
-}
-
-const translateText = async (text: string, lang: string) => {
-  const urlParams = new URLSearchParams({
-    string: text,
-    from_lang: defaultLocale,
-    to_lang: lang,
-  })
-  const response = await fetch(`https://api.datpmt.com/api/v2/dictionary/translate?${urlParams.toString()}`)
-  if (response.status !== 200) {
-    console.error(response.statusText)
-    process.exit(1)
+    if (currentChunk) {
+      const tmp = await translateText(currentChunk, lang)
+      if (tmp) appendFileSync(destinationPath, tmp, 'utf8')
+    }
+    let translatedContent = await readFileSync(destinationPath, 'utf8')
+    codeBlocks.forEach((match, _) => {
+      translatedContent = translatedContent.replace('[[CODE_BLOCK]]', match[0])
+    })
+    writeFileSync(destinationPath, translatedContent, 'utf8')
+    console.log(`Blog translated: ${file}`)
   }
-  return await response.json()
 }
-
-translateBlogFiles()
--- generate_locale_translations.tsx
+++ generate_locale_translations.tsx
@@ -1,24 +1,11 @@
 import fs from 'fs'
 import { dump, load } from 'js-yaml'
 import path from 'path'
+import { translateText } from './translate'
 
 const newLocale = ['es', 'fr']
 
 const localePath = path.join(process.cwd(), 'locales', 'en.yml')
-
-const translateText = async (text: string, lang: string) => {
-  const urlParams = new URLSearchParams({
-    string: text,
-    from_lang: 'en',
-    to_lang: lang,
-  })
-  const response = await fetch(`https://api.datpmt.com/api/v2/dictionary/translate?${urlParams.toString()}`)
-  if (response.status !== 200) {
-    console.error(response.statusText)
-    process.exit(1)
-  }
-  return await response.json()
-}
 
 console.log('Loading data from locales/en.yml...')
 const enLocaleContent = fs.readFileSync(localePath, 'utf8')
@@ -27,11 +14,9 @@ const data = load(enLocaleContent) as { [p: string]: string }
 for (const lang of newLocale) {
   const newData = { ...data }
   console.log(`Translating en.yml to ${lang}.yml...`)
-  await Promise.all(
-    Object.keys(data).map(async (key) => {
-      newData[key] = await translateText(data[key], lang)
-    }),
-  )
+  for (const key in data) {
+    newData[key] = await translateText(data[key], lang)
+  }
   const newLocalePath = path.join(process.cwd(), 'locales', lang + '.yml')
   fs.writeFileSync(newLocalePath, dump(newData), 'utf8')
   console.log(`Wrote ${lang}.yml to locales directory.`)
