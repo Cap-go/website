493df6c0e218dc10a1c9816095977834dd95e1a4
Rishi Raj Jain <jain71000@gmail.com>
2024-10-05 09:26:49 +0530

    use dotenv with OPENAI API

--- translate.tsx
+++ translate.tsx
@@ -1,41 +1,41 @@
-// import 'dotenv/config'
-
-// export const translateText = async (text: string, lang: string) => {
-//   const response = await fetch('https://api.openai.com/v1/chat/completions', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-//     },
-//     body: JSON.stringify({
-//       model: 'gpt-4o-mini',
-//       messages: [
-//         {
-//           role: 'system',
-//           content: `Only respond with the translation of the text. No other or unrelated text or characters.`,
-//         },
-//         {
-//           role: 'user',
-//           content: `Translate the following text to ${lang} locale:\n\n${text}`,
-//         },
-//       ],
-//       max_tokens: 4000,
-//     }),
-//   })
-//   if (response.status !== 200) {
-//     console.error(response.statusText)
-//     process.exit(1)
-//   }
-//   const data = await response.json()
-//   return data.choices[0].message.content.trim()
-// }
+import 'dotenv/config'
 
 export const translateText = async (text: string, lang: string) => {
-  const params = new URLSearchParams({
-    string: text,
-    to_lang: lang,
-    from_lang: 'en',
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: 'gpt-4o-mini',
+      messages: [
+        {
+          role: 'system',
+          content: `Only respond with the translation of the text. No other or unrelated text or characters.`,
+        },
+        {
+          role: 'user',
+          content: `Translate the following text to ${lang} locale:\n\n${text}`,
+        },
+      ],
+      max_tokens: 4000,
+    }),
   })
-  const response = await fetch(`https://api.datpmt.com/api/v2/dictionary/translate?${params.toString()}`)
-  return await response.json()
+  if (response.status !== 200) {
+    console.error(response.statusText)
+    process.exit(1)
+  }
+  const data = await response.json()
+  return data.choices[0].message.content.trim()
 }
+
+// export const translateText = async (text: string, lang: string) => {
+//   const params = new URLSearchParams({
+//     string: text,
+//     to_lang: lang,
+//     from_lang: 'en',
+//   })
+//   const response = await fetch(`https://api.datpmt.com/api/v2/dictionary/translate?${params.toString()}`)
+//   return await response.json()
+// }
