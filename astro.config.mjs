c22b97b82425ff9c8f67e1473cbdfc5dd84224e5
Rishi Raj Jain <jain71000@gmail.com>
2024-10-06 11:49:54 +0530

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -149,12 +149,12 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
-    host: '0.0.0.0',
     open: true,
+    host: '0.0.0.0',
   },
   preview: {
     port: 3000,
-    host: '0.0.0.0',
     open: true,
+    host: '0.0.0.0',
   },
 })
