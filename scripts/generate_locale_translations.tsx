6143546ab9a3dcd33af00684ae08a0255872b78a
Rishi Raj Jain <jain71000@gmail.com>
2024-10-14 11:58:47 +0000

    handle failing

--- scripts/generate_locale_translations.tsx
+++ scripts/generate_locale_translations.tsx
@@ -17,14 +17,34 @@ for (const lang of locales) {
   const newLocalePath = path.join(process.cwd(), 'locales', lang + '.yml')
   fs.writeFileSync(newLocalePath, ``, 'utf8')
   const keys = Object.keys(data)
+  const failedTranslations: { [key: string]: string } = {}
   for (let i = 0; i < keys.length; i += batchSize) {
     const batchKeys = keys.slice(i, i + batchSize)
-    const translations = await Promise.all(batchKeys.map((key) => translateText(data[key], lang)))
+    const translations = await Promise.all(batchKeys.map((key) => translateText(data[key], lang).catch(() => null)))
     for (let j = 0; j < batchKeys.length; j++) {
-      const tmp = { [batchKeys[j]]: translations[j] }
-      fs.appendFileSync(newLocalePath, dump(tmp).replaceAll('1$', '$1'), 'utf8')
+      if (translations[j] === null) {
+        failedTranslations[batchKeys[j]] = data[batchKeys[j]]
+      } else {
+        const tmp = { [batchKeys[j]]: translations[j] }
+        fs.appendFileSync(newLocalePath, dump(tmp).replaceAll('1$', '$1'), 'utf8')
+      }
     }
     await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+  const failedKeys = Object.keys(failedTranslations)
+  if (failedKeys.length > 0) {
+    console.log(`Retrying failed translations for ${lang}.yml...`)
+    for (let i = 0; i < failedKeys.length; i += batchSize) {
+      const batchKeys = failedKeys.slice(i, i + batchSize)
+      const translations = await Promise.all(batchKeys.map((key) => translateText(failedTranslations[key], lang).catch(() => null)))
+      for (let j = 0; j < batchKeys.length; j++) {
+        if (translations[j] !== null) {
+          const tmp = { [batchKeys[j]]: translations[j] }
+          fs.appendFileSync(newLocalePath, dump(tmp).replaceAll('1$', '$1'), 'utf8')
+        }
+      }
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
   }
   console.log(`Wrote ${lang}.yml to locales directory.`)
   console.log('Done.\n')
