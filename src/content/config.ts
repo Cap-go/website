91bc5cbc39eea6b3c5939a7d0a6e8cedd7151fd5
Martin Donadieu <martindonadieu@gmail.com>
2025-01-08 00:44:36 +0000

    fix: lang configs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,8 @@
-import { docsSchema } from '@astrojs/starlight/schema'
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
-import { docsLoader } from "@astrojs/starlight/loaders"
+import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders"
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 }
