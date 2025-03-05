8ca5705655229cbc4aa1c3db2a92621e25a26d00
Martin Donadieu <martindonadieu@gmail.com>
2025-03-05 14:33:19 +0000

    fix: translate

--- src/components/pricing/Plans.vue
+++ src/components/pricing/Plans.vue
@@ -108,7 +108,7 @@ function descToEmoji(desc: string) {
             <li class="flex items-center">
               <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-1 text-blue-600 shrink-0" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="M23 23c-5.656 0-7.858-6.41-7.949-6.684C15.034 16.265 13.208 11 9 11c-2.757 0-5 2.243-5 5s2.243 5 5 5c1.588 0 3.013-.732 4.237-2.176l1.526 1.293C13.164 22.003 11.172 23 9 23c-3.86 0-7-3.14-7-7s3.14-7 7-7c5.656 0 7.858 6.41 7.949 6.684C16.966 15.735 18.792 21 23 21c2.757 0 5-2.243 5-5s-2.243-5-5-5c-1.588 0-3.013.732-4.237 2.176l-1.526-1.293C18.836 9.997 20.828 9 23 9c3.86 0 7 3.14 7 7s-3.14 7-7 7"/></svg>
               <span>
-                Unlimited Live Updates
+                {{ m.unlimited_live_updates() }}
               </span>
             </li>
             <li class="flex items-center">
