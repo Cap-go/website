9a1b6d7572c7f1c7bc0d7472cb6c841a1067b299
Rishi <jain71000@gmail.com>
2025-04-29 14:09:12 +0530

    fix removal script

--- scripts/repair_sitemap.tsx
+++ scripts/repair_sitemap.tsx
@@ -1,11 +1,8 @@
 import { XMLBuilder, XMLParser } from 'fast-xml-parser'
 import { existsSync, readFileSync, writeFileSync } from 'fs'
 import { join } from 'path'
-import { locales } from '../src/services/locale'
 
-const table: any = {}
 const sitemapPath = join('dist', 'sitemap-0.xml')
-const localeRegex = new RegExp(`^\\/(${locales.join('|')})\\/`)
 
 if (existsSync(sitemapPath)) {
   const xmlData = readFileSync(sitemapPath, 'utf-8')
@@ -14,14 +11,10 @@ if (existsSync(sitemapPath)) {
   const initialUrls = copiedUrls.length
   const urls = jsonObj.urlset.url.map((u: any) => u.loc).filter((i: any) => i.includes('/blog/'))
   urls.forEach((url: string) => {
-    const slug = new URL(url).pathname.replace(localeRegex, '/')
-    if (!table[slug]) table[slug] = {}
-    locales.forEach((locale: any) => {
-      const localizedPath = join('dist', locale, ...slug.split('/').filter(Boolean))
-      if (!existsSync(localizedPath)) {
-        if (copiedUrls.indexOf(url) !== -1) copiedUrls.splice(copiedUrls.indexOf(url), 1)
-      }
-    })
+    const localizedPath = join('dist', ...new URL(url).pathname.split('/').filter(Boolean))
+    if (!existsSync(localizedPath) && copiedUrls.indexOf(url) !== -1) {
+      copiedUrls.splice(copiedUrls.indexOf(url), 1)
+    }
   })
   console.log(`Removed ${initialUrls - copiedUrls.length} urls that were not found.`)
   jsonObj.urlset.url = copiedUrls.map((loc: string) => ({ loc }))
