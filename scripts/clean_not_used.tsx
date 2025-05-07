aee3d9a0e7f6e79ed94d226dba487f7e33ca75a0
Rishi <jain71000@gmail.com>
2025-05-08 03:54:53 +0530

    use Astro.locals.runtimeConfig

--- scripts/clean_not_used.tsx
+++ scripts/clean_not_used.tsx
@@ -4,16 +4,23 @@ import path from 'path'
 
 const unusedFiles = new Set<string>()
 const [publicFiles, srcFiles] = await Promise.all([
-  fg(['public/**/*'], { dot: true, onlyFiles: true, ignore: ['public/_headers', 'public/_redirects', 'public/fonts/**/*', 'public/deepLink/**/*'] }),
+  fg(['public/**/*'], {
+    dot: true,
+    onlyFiles: true,
+    ignore: ['public/_headers', 'public/_redirects', 'public/fonts/**/*', 'public/deepLink/**/*', 'public/react_native.webp', 'public/flutter.webp'],
+  }),
   fg(['src/**/*', 'astro.config.mjs'], { dot: true, onlyFiles: true }),
 ])
+
 const publicFileNames = publicFiles.map((file) => path.relative('public', file))
 const srcFileContents = srcFiles.map((srcFile) => readFileSync(srcFile, 'utf8'))
+
 for (const publicFileName of publicFileNames) {
   const fileName = path.basename(publicFileName)
   const isUsed = srcFileContents.some((content) => content.includes(fileName))
   if (!isUsed) unusedFiles.add(path.join('public', publicFileName))
 }
+
 for (const unusedFile of unusedFiles) {
   try {
     unlinkSync(unusedFile)
