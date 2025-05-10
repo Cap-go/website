0167681eaee72f2bd6d48a897e2db0e37d989ba9
Rishi <jain71000@gmail.com>
2025-05-10 14:53:25 +0530

    try with more concurrency

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ import { defaultLocale, localeNames, locales } from './src/services/locale'
 export default defineConfig({
   trailingSlash: 'always',
   site: `https://${config.base_domain.prod}`,
+  build: {
+    concurrency: 2,
+  },
   env: {
     validateSecrets: true,
     schema: {
